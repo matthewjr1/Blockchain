2b12996a2abfff8403ee1dae242a9fe4960feff3
matthewjr@qcnetworks.ca <matthewjr@qcnetworks.ca>
2022-12-31 14:44:39 -0800

    Start of Project - First Build

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
+
+    public static ArrayList<Thread> System_Threads = new ArrayList<>();
+    public static Random random = new Random();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Start();
+    }
+
+    public static void Start(){
+        try {
+            while (true){
+                System.out.println("WORKING");
+                Thread.sleep(10000);
+            }
+        }catch (Exception ex){
+
+        }
     }
 }
+
+
